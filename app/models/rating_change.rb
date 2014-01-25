4c2fb5ff7d7dcf78b477f669a729239aa132eed6
Rohit Nair <admin@rohitnair.net>
2014-01-25 17:34:26 +0400

    Code to rollback rating mistakes

--- app/models/rating_change.rb
+++ app/models/rating_change.rb
@@ -1,12 +1,26 @@
 class RatingChange < ActiveRecord::Base
   belongs_to :player
   belongs_to :match
-  
+
   def self.apply_ratings
     changes = RatingChange.includes(:player).where(applied: false).order("id ASC")
     changes.each do |c|
       c.player.rating += c.change
       c.player.save
+      c.applied = true
+      c.save
+    end
+  end
+
+  def self.rollback_rating(rc)
+    rc.player.rating -= rc.change
+    rc.player.save
+    rc.destroy
+  end
+
+  def self.rollback_ratings
+    RatingChange.all.each do |rc|
+      RatingChange.rollback_rating(rc)
     end
   end
 end
